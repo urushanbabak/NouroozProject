6d26695860790dc8338172b8b6465f22a17bdf84
urushanbabak <babakroshnai350@gmail.com>
2022-03-18 08:51:42 -0700

    Define methodes: create,insert,remove,replace and checkArray

--- src/us/ilgar/Main.java
+++ src/us/ilgar/Main.java
@@ -1,9 +1,11 @@
 package us.ilgar;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 enum Command {
-    create, insert, remove, replace;
+    create, insert, remove, replace, unknown;
 }
 
 public class Main {
@@ -16,7 +18,11 @@ public class Main {
             System.out.println("Write Your Command:");
 
             String input = scanner.next();
-            terminalCommandToEnum(input);
+            if (terminalCommandToEnum(input).equals(Command.unknown)) {
+                System.out.println("Unknown Command!");
+            }
+
+            checkCommand(terminalCommandToEnum(input));
             i++;
         } while (true);
     }
@@ -25,7 +31,7 @@ public class Main {
         input = input.toLowerCase();
         Command command = Command.create;
         switch (input) {
-            case "creat":
+            case "create":
                 command = Command.create;
                 break;
             case "insert":
@@ -38,14 +44,40 @@ public class Main {
                 command = Command.replace;
                 break;
             default:
-                return null;
+                command = Command.unknown;
 
         }
         return command;
     }
 
-    public static void create() {
+    public static void checkCommand(Command command) {
+        switch (command) {
+            case create:
+                create();
+                break;
+            case insert:
+                insert();
+                break;
+            case remove:
+                remove();
+                break;
+            case replace:
+                replace();
+                break;
+            default:
+                ;
+        }
+    }
 
+    public static void create() {
+        System.out.println("Define Your Array");
+        String[] array = new String[10];
+        do {
+            Scanner creatScanner = new Scanner(System.in);
+            String stringArray = creatScanner.next();
+            array = stringArray.split(",");
+            System.out.print(Arrays.toString(array));
+        } while (checkArray(array));
     }
 
     public static void insert() {
@@ -58,6 +90,25 @@ public class Main {
 
     public static void replace() {
 
+    }
+
+    public static boolean checkArray(String[] array) {
+        if (array[1].matches("\\d")) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].matches("\\D")) {
+                    return false;
+                }
+            }
+        }
+        else if (array[1].matches("\\d")) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].matches("\\D")) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
 
