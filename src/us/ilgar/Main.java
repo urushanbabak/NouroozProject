840619d12abe03092795cf6e0dac7eabbd7dc339
urushanbabak <babakroshnai350@gmail.com>
2022-03-18 00:02:06 -0700

    Creat Scanner and  to give Command form Terminal

--- src/us/ilgar/Main.java
+++ src/us/ilgar/Main.java
@@ -1,4 +1,11 @@
 package us.ilgar;
+import java.util.Scanner;
 
 public class Main {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hi Dear. What Can I do?!");
+        String input = scanner.next();
+
+    }
 }
