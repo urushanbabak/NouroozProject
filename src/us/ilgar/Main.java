9d406572395dee14721160b408b01b408da4a1f2
urushanbabak <babakroshnai350@gmail.com>
2022-03-18 03:16:51 -0700

    Set example

--- src/us/ilgar/Main.java
+++ src/us/ilgar/Main.java
@@ -1,11 +1,20 @@
 package us.ilgar;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i = 0;
         System.out.println("Hi Dear. What Can I do?!");
-        String input = scanner.next();
+        do {
+            if (i > 1) {
+                System.out.print("Your Array:");
+                System.out.println("[hi,hello,how are you]");
+            }
+            String input = scanner.next();
+            i++;
+        } while (true);
 
     }
 }
